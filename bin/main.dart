b74a6babad182e0976800a395c0c172025b040ec
andreduarte99 <andre.duarte16@outlook.com>
2025-04-21 11:44:29 -0300

    finalizando primeira aula

--- bin/main.dart
+++ bin/main.dart
@@ -9,5 +9,12 @@ void main() {
 requestData() {
   String url =
       "https://gist.githubusercontent.com/andreduarte99/0ff5702b462e1e31941de213311efb0e/raw/b5797aa29f77f4dd8a73e498cebaa69fc077902a/accounts.json";
-  print(get(Uri.parse(url)));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then(
+    (Response response) {
+      print(response);
+      print(response.body);
+    },
+  );
 }
