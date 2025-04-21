d061a1df88cd6512642e90caa1ee112c1852329d
andreduarte99 <andre.duarte16@outlook.com>
2025-04-20 23:04:26 -0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart';
+
+void main() {
+  // print("Hello, World!");
+
+  requestData();
+}
+
+requestData() {
+  String url =
+      "https://gist.githubusercontent.com/andreduarte99/0ff5702b462e1e31941de213311efb0e/raw/b5797aa29f77f4dd8a73e498cebaa69fc077902a/accounts.json";
+  print(get(Uri.parse(url)));
+}
